c0603ea23f09ecfb88f6d27b2e7c07a61d8456c7
vivekoraon4321@gmail.com <contact@spareme.eu.org>
2026-03-31 11:48:15 +0530

    db and tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+import {ENV} from "./env.js";
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL);
+        console.log("✅ Connected to MongoDB:", conn.connection.host);
+    }
+    catch(error){
+        console.error("❌ Error connecting to MongoDB", error);
+        process.exit(1); // 0 means success, 1 means failure
+    }
+}
